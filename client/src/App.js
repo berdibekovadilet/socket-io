859be68f7496ba59ae484134a3d5e70ff5325058
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-12-23 14:31:55 +0600

    added layout to chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="formbg">
+        <div className="row">
+          <input placeholder="Room Number..." />
+          <button>Join Room</button>
+        </div>
+        <div className="row">
+          <input placeholder="Message..." />
+          <button>Send Message</button>
+        </div>
+        <h2>Message:</h2>
+      </form>
     </div>
   );
 }
